32750f9e27e879adeb5493ac04bdcd165863e178
Samma <46258134+Samunter@users.noreply.github.com>
2019-12-18 01:11:00 -0800

    basically done

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # techdegree-project-6
- Static Node.js and Express Site
+
+Static Node.js and Express Site
+
+This project uses Express, Node.js, and npm to create a digital portfolio. It
+serves webpages that demonstrate projects, including images and links to live demos.
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const data = require('./data.json');
+
+const app = express();
+
+// Tell express which template engine to use
+app.set('view engine', 'pug');
+
+// Add static asset middleware and route static server to /static
+app.use('/static', express.static('public'));
+
+app.get('/', (_req, res) => {
+  res.render('index', data);
+});
+
+app.get('/about', (_req, res) => {
+  res.render('about');
+});
+
+// Load the project page. URL specifies which project, pug template is given
+// access to that project's json data
+// If the specified project doesn't exist, don't load page and execute next middleware
+app.get('/projects/:id', (req, res, next) => {
+  const id = req.params.id;
+  const project = data.projects.filter(project => project.id === id)[0];
+
+  if (!project) {
+    next();
+  }
+
+  console.log('project', project); // Sanity check
+  res.render('project', project);
+});
+
+// If the URL doesn't match a get request, create an error and pass it to error
+// handling middleware
+app.use((_req, _res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handling middleware. Renders 'error' pug template
+app.use((err, _req, res, _next) => {
+  err.status = err.status || 500; // Ensure there's always a status code.
+  err.message = err.message || 'Internal Server Error'; // Ensure there's always an error message.
+
+  res.status(err.status);
+  res.render('error', { error: err });
+});
+
+// Initialize server
+app.listen(3000, () => {
+  console.log('Server Started! WoooooooooooooooooooooOOO!');
+});
